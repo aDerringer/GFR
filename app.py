a0b4a3ffc74558b7139fffac80a9f767d504c738
Andrew Derringer <derringa@flip3.engr.oregonstate.edu>
2019-10-10 18:49:51 -0700

    Push old repository

---
+++ app.py
@@ -0,0 +1,28 @@
+# http://pythonprogramminglanguage.com/pyqt5-hello-world/
+import sys
+from PyQtX import QtCore, QtWidgets
+from PyQtX.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
+from PyQtX.QtCore import QSize    
+
+class HelloWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(640, 480))    
+        self.setWindowTitle("Hello world - pythonprogramminglanguage.com") 
+
+        centralWidget = QWidget(self)          
+        self.setCentralWidget(centralWidget)   
+
+        gridLayout = QGridLayout(self)     
+        centralWidget.setLayout(gridLayout)  
+
+        title = QLabel("Hello World from PyQt", self) 
+        title.setAlignment(QtCore.Qt.AlignCenter) 
+        gridLayout.addWidget(title, 0, 0)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = HelloWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
